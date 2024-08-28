13fe56d9956868fba9a0bbed3d6e9e2494bffa28
Cahandarov <cahandarov@gmail.com>
2024-08-28 20:13:36 +0400

    first commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
